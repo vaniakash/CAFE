3d5db3cb8330a5e0465e57a9bf8f2dc2606a8287
Akash Rana <akashrana@Akashs-MacBook-Air.local>
2025-04-26 01:50:17 +0530

    Update Vercel and Vite configurations for deployment

--- cafe/vite.config.js
+++ cafe/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    assetsDir: 'assets',
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+  }
 })
